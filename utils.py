c7d0ea0cb6f645a114c391e08aba468f0ee2ce25
Jason <2014145110@yonsei.ac.kr>
2021-07-20 13:33:59 +0900

    Add utils.py

---
+++ utils.py
@@ -0,0 +1,8 @@
+import random, torch
+
+
+def count(output, target):
+    with torch.no_grad():
+        predict = torch.argmax(output, 1)
+        correct = (predict == target).sum().item()
+        return correct
